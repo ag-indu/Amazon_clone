676111666401cd8ff97f709df45c447ea223f0b9
ag-indu <indulekhaag@gmail.com>
2025-06-23 21:08:27 +0530

    Use modules and import cart from cart.js and product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,9 @@
-let videoGrid = ''
-let cartCount = 0;
+let videoGrid = '';
 let cartCountText = document.querySelector('.cart-item-number');
-cartCountText.innerHTML = cartCount;
+cartCountText.innerHTML = 0;
+import {cart} from '../Data/cart.js'
+import { products } from '../Data/product.js';
+
 products.forEach((product)=>{
     const html = `<article class="item-description">
     <div class="item-image-container">
@@ -27,7 +29,7 @@ products.forEach((product)=>{
             <option value="10">10</option>
         </select>
     </div>
-    <button class="cart-button">Add to Cart</button>
+    <button class="cart-button" data-product-id="${product.id}">Add to Cart</button>
 </article>`
   videoGrid += html;
 })
@@ -36,7 +38,29 @@ document.querySelector('.products').innerHTML = videoGrid
 
 document.querySelectorAll('.cart-button').forEach((button)=>{
     button.addEventListener('click',()=>{
-        cartCount++;
+        
+        const productId = button.dataset.productId;
+        let matchingitem;
+        cart.forEach((item)=>{
+            if(productId===item.productId){
+                matchingitem = item;
+            }
+        })
+
+        if(!matchingitem){
+                cart.push({
+                productId,
+                quantity:1
+            });
+        }
+        else{
+            matchingitem.quantity++;
+        }
+        let cartCount = 0;
+        console.log(cart);
+        cart.forEach((item)=>{
+            cartCount += item.quantity;
+        })
         cartCountText.innerHTML = cartCount;
     });
 
